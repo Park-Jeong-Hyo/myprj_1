ab78fe7ab43a894ab4daa5a0534426c44cdd7c61
ParkJeongHyo <qkrwjdgy123@naver.com>
2023-03-13 15:14:56 +0900

    삭제 기능 추가

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
 const $btnAdd = document.getElementById('btnAdd');
 $btnAdd.addEventListener('click', () => console.log('클릭'));
+
+const $btnDel = document.getElementById('btnDel');
+$btnDel.addEventListener('click', () => console.log('삭제'));
